6cec479e3a3071facc9860c86dee8326f22cbedf
HoangHoi <hoanghoi1310@gmail.com>
2017-12-10 11:11:05 +0700

    Add right info fix top

--- resources/assets/js/core/bootstrap.js
+++ resources/assets/js/core/bootstrap.js
@@ -1,27 +1,11 @@
 
 window._ = require('lodash');
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
 try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
 } catch (e) {}
-
-// window.axios = require('axios');
-
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
@@ -31,26 +15,33 @@ if (token) {
             'X-CSRF-TOKEN': token.content,
         }
     });
-    // window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
-
 window.numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+window.fixTop = (element, distanceToTopOrigin = 500, scrollElement = null, distanceToTop = 0) => {
+    if (scrollElement) {
+        scrollElement = $(scrollElement);
+    } else {
+        scrollElement = $(window);
+    }
+
+    let handleScroll = () => {
+        let scrollTop = $(window).scrollTop(),
+            elementOffset = $(element).offset().top;
+        if (scrollTop > distanceToTopOrigin) {
+            $(element).css({'top' : `${distanceToTop + scrollTop - distanceToTopOrigin}px`});
+        } else {
+            $(element).css({'top' : ''});
+        }
+    };
+
+    $(scrollElement).on('scroll', handleScroll);
+    handleScroll();
+}
+
+fixTop('.right-menu-info');
---
+++ resources/views/includes/right-menu-info.blade.php
@@ -0,0 +1,17 @@
+<div class="panel panel-default right-menu-info">
+    <div class="panel-body">
+        <div>
+            <div class="menu-title">Liên hệ</div>
+            <div class="menu-body">
+                <div class="phone-number">
+                    <span class="info-title"><i class="fa fa-phone" aria-hidden="true"></i></span>
+                    <span class="info-content">024 66 525 595</span>
+                </div>
+                <div class="address">
+                    <span class="info-title"><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                    <span class="info-content">Xóm 1, Cổ Điển, Hải Bối, Đông Anh, Hà Nội</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
